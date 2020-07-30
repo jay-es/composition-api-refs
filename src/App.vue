db135c4ce8e2a75ecff331c698ef8ca241d3740a
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2020-07-31 02:17:59 +0900

    ref 確認

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <img ref="imgRef" alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld ref="componentRef" msg="Hello Vue 3.0 + Vite" />
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
     HelloWorld,
   },
-}
+  setup() {
+    const imgRef = ref<HTMLImageElement>()
+
+    console.log(imgRef.value) // -> undefined
+
+    onMounted(() => {
+      console.log(imgRef.value) // <img>
+      console.log(imgRef.value?.clientHeight) // -> 0
+
+      imgRef.value?.addEventListener('load', () => {
+        console.log(imgRef.value?.clientHeight) // -> 200
+      })
+    })
+
+    const componentRef = ref<InstanceType<typeof HelloWorld>>()
+
+    console.log(componentRef.value) // undefined
+
+    onMounted(() => {
+      console.log(componentRef.value) // コンポーネントの Proxy
+
+      if (!componentRef.value) return
+
+      console.log(componentRef.value.$el) // テキストノード
+
+      console.log(componentRef.value.$props.msg) // props で渡した値
+      console.log(componentRef.value.$data.count) // コンポーネントの $data の値
+
+      componentRef.value.count++ // バッドプラクティス！
+      console.log(componentRef.value.count) // 増えている
+    })
+
+    return { imgRef, componentRef }
+  },
+})
 </script>
